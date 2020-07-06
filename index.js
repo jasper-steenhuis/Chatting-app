ad661c98f08d94ce8c809ac7bac81440c079d27b
Jasper Steenhuis <jasper.steenhuis@student.nhlstenden.com>
2020-07-06 13:40:09 +0200

    added usernames
    fixed the username issue

--- index.js
+++ index.js
@@ -7,27 +7,37 @@ var addresses = [];
 
 
 for (var k in interfaces) {
-    for(var k2 in interfaces[k]){
+    for (var k2 in interfaces[k]) {
         var address = interfaces[k][k2];
-        if(address.family === 'IPv4' && !address.internal){
+        if (address.family === 'IPv4' && !address.internal) {
             addresses.push(address.address);
         }
     }
 }
 console.log(addresses);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 
 });
-io.on('connection', (socket) =>{
-    socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);    
+io.on('connection', (socket) => {
+    console.log("a user has connected");
+
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+        console.log(socket.username);
     });
+
+    socket.on('new_message', (data) => {
+        io.sockets.emit('new_message', { message: data.message, username: socket.username });
+        console.log(socket.username);
+        console.log(data.username);
+    });
+
 });
-io.on('disconnect',() => {
+io.on('disconnect', () => {
     console.log('user disconnected');
 });
-http.listen(3000,addresses,()=> {
+http.listen(3000, addresses, () => {
     console.log('listening on ' + addresses);
 });
