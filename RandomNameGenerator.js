1302046597ae96446e9caa06bb708662e77e3805
Jasper <steenhuisjasper@hotmail.com>
2020-07-06 09:38:55 +0200

    init commit

---
+++ RandomNameGenerator.js
@@ -0,0 +1,6 @@
+
+
+function RandomName(){
+    return "AAAA";
+}
+exports.randomName = RandomName();
---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+const random = require('./RandomNameGenerator');
+
+
+for (var k in interfaces) {
+    for(var k2 in interfaces[k]){
+        var address = interfaces[k][k2];
+        if(address.family === 'IPv4' && !address.internal){
+            addresses.push(address.address);
+        }
+    }
+}
+console.log(addresses);
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+
+});
+io.on('connection', (socket) =>{
+    socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)    
+    });
+    socket.emit('chat message',random.randomName);
+});
+io.on('disconnect',() => {
+    console.log('user disconnected');
+});
+http.listen(3000,addresses,()=> {
+    console.log('listening on ' + addresses);
+});
